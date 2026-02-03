a696b877ff22b628ee26dd822ca29080171babc6
osmanjosue <osmanjosue@gmail.com>
2026-02-02 22:48:43 -0600

    Seccion 26 => Cambiar el URL despues de una autenticacion

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -18,12 +18,17 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-      {
-        (status === 'not-authenticated') 
-          ? <Route path="/auth/*" element={<LoginPage />} />
-          : <Route path="/*" element={<CalendarPage />} />
-      }
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
+      {status === 'not-authenticated' ? (
+        <>
+          <Route path="/auth/*" element={<LoginPage />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<CalendarPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
+      )}
     </Routes>
   );
 };
