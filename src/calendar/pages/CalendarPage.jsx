e4ebe3ce9127b10373299b26ceb6f5181d24a0a4
osmanjosue <osmanjosue@gmail.com>
2026-02-06 15:34:02 -0600

    Seccion 27 => Cambiar el color de los eventos segun usuario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -5,9 +5,11 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { NavBar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from '../';
 
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
+
+  const {user} = useAuthStore();
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
@@ -16,8 +18,11 @@ export const CalendarPage = () => {
   );
 
   const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const isMyEvent = (user.uid === event.user._id) || (user.uid === event.user.uid);   
+    
     const style = {
-      backgroundColor: '#347CF7',
+      backgroundColor: isMyEvent ? '#347CF7' : '#465660',
       borderRadius: '0px',
       opacity: 0.8,
       color: 'white',
