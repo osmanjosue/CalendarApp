88f51903b7cc4b3e4515182d044c0c1aae71a192
osmanjosue <osmanjosue@gmail.com>
2026-02-05 22:45:42 -0600

    Seccion 27 => Cargar los eventos al store

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store";
 import calendarApi from "../api/calendarApi";
 import { convertEventsToDateEvents } from "../helpers";
 
@@ -36,7 +36,8 @@ export const useCalendarStore = () => {
         try {
             const {data} = await calendarApi.get('/events'); 
             const events = convertEventsToDateEvents( data.eventos );
-            console.log({events})
+            dispatch( onLoadEvents( events ) );
+            console.log(events)
         } catch (error) {
             console.log('Error cargando eventos')
             console.log(error);
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -2,24 +2,28 @@ import {
     createSlice
 } from '@reduxjs/toolkit';
 import { addHours } from 'date-fns';
+import { is } from 'date-fns/locale';
 
-const temEvent = {
-    _id: new Date().getTime(),
-    title: 'Cumpleaños del Jefe',
-    notes: 'Hay que comprar el pastel',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: '#fafafa',
-    user: {
-        _id: '123',
-        name: 'Fernando',
-    },
-}
+// const temEvent = {
+//     _id: new Date().getTime(),
+//     title: 'Cumpleaños del Jefe',
+//     notes: 'Hay que comprar el pastel',
+//     start: new Date(),
+//     end: addHours(new Date(), 2),
+//     bgColor: '#fafafa',
+//     user: {
+//         _id: '123',
+//         name: 'Fernando',
+//     },
+// }
 
 export const calendarSlice = createSlice({
     name: 'calendar',
     initialState: {
-        events: [temEvent],
+        isLoadingEvents: true,
+        events: [
+            // temEvent
+        ],
         activeEvent: null
     },
     reducers: {
@@ -43,10 +47,20 @@ export const calendarSlice = createSlice({
                 state.events = state.events.filter(event => event._id !== state.activeEvent._id);
                 state.activeEvent = null;
             }
+        },
+        onLoadEvents: (state, { payload = [] }) => {
+            state.isLoadingEvents = false;
+            // state.events = payload;
+            payload.forEach(event => {
+                const exists = state.events.some(dbEvent => dbEvent.id === event._id);
+                if (!exists) {
+                    state.events.push(event);
+                }
+            });
         }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onLoadEvents } = calendarSlice.actions;
